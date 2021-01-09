b3e17b6bed48ac3c78cd5c39f53ff648b0c6776f
Cherepanov Igor <zigorewslike@gmail.com>
2021-01-09 18:30:44 +0800

    Динамическая смена даты с анимацией

--- app.js
+++ app.js
@@ -9,44 +9,109 @@ new Vue({
                     { 
                         text: new Date(Date.now() - 86400000).toISOString().substring(0,10),
                         style:{
-                            color: 'white',
+                            //color: 'white',
                             cursor: 'pointer',
                         }
                     },
                     { 
                         text: new Date(Date.now()).toISOString().substring(0,10),
                         style:{
-                            color: '#7e7cff',
-                            textDecoration: 'underline',
-                            cursor: 'pointer',
+                            //color: '#7e7cff',
+                            //textDecoration: 'underline',
+                            //cursor: 'pointer',
                         }
                     },
                     { 
                         text: new Date(Date.now() + 86400000).toISOString().substring(0,10),
                         style:{
-                            color: 'white',
+                            //color: 'white',
                             cursor: 'pointer',
                         } 
                     },
                 ],
+                change: false,
             },
             roverName: 'curiosity',
+            posX: 0,
         }
-    },methods: {
+    },
+    updated: function () {
+
+    },
+    watch: {
+        posX: function(newValue) {
+            for(let i=0; i<3; i++){
+                TweenLite.to(cir_shape[i].graphics.command, 0.8, { x: newValue+(clientWidth/6* (i*2+1)), onComplete: function() { 
+                    cir_shape[i].graphics.command.x = (clientWidth/6* (i*2+1)); 
+                    cir_shape[0].lock = false; 
+                } ,onUpdate : function () {
+                    if(cir_shape[i].graphics.command.x < 0){
+                        cir_shape[i].graphics.command.x += clientWidth;
+                    }else if(cir_shape[i].graphics.command.x > clientWidth){
+                        cir_shape[i].graphics.command.x -= clientWidth;
+                    }
+                }});
+            }
+            console.log("DWIJJ : " + this.posX);
+            if(this.posX == 0){
+                this.posX = clientWidth/3;
+            }else if(this.posX == -1){
+                this.posX = -clientWidth/3;
+            }
+        },
+    },
+    methods: {
         animCanvas(index){
-            cir_shape[index].anim = true;
+            if(!cir_shape[0].lock && !(index == 2 && this.dateStep == 0)){
+                cir_shape[index].anim = true; 
+            }
+            else
+                cir_shape[index].anim = false;
+            
         },
         animCanvasOff(index){
             cir_shape[index].anim = false;
         },
         nextDate(index){
-            if(index == 0){
-                this.dateStep -= 1;
-            }else{
-                this.dateStep += 1;
-            }
+            if(!cir_shape[0].lock){
+                cir_shape[index].anim = false;
+                if(index == 0){
+                    this.dateDivs.change = true;
+                    cir_shape[0].lock = true;
+                    this.dateStep += 1;
+                    this.dateDivs.change = true;
+                    cir_shape[0].lock = true; 
+                    if(this.posX != 0){
+                        this.posX = 0;
+                    }else{
+                        this.posX += clientWidth/3;
+                    }
+                }else if(index == 2 && this.dateStep > 0){
+                    this.dateDivs.change = true;
+                    cir_shape[0].lock = true;
+                    this.dateStep -= 1;
+                    this.dateDivs.change = true;
+                    cir_shape[0].lock = true; 
+                    if(this.posX != -1){
+                        this.posX = -1;
+                    }else{
+                        this.posX += clientWidth/3;
+                    }
+                }
+                for(let i=0;i<3;i++){
+                    this.dateDivs.collection[i].text = new Date(Date.now() - 86400000 * (this.dateStep+(2-i-1))).toISOString().substring(0,10);
+                }
+                if(index == 0){
+                    
+                }else if(index == 2){
+                    
+                }
+                
+            } 
+        },
+        updateDate(){
             for(let i=0;i<3;i++){
-                this.dateDivs.collection[i].text = new Date(Date.now() + 86400000 * (this.dateStep+(i-1))).toISOString().substring(0,10);
+                this.dateDivs.collection[i].text = new Date(Date.now() - 86400000 * (this.dateStep+(2-i-1))).toISOString().substring(0,10);
             }
         }
     }
--- canvas.js
+++ canvas.js
@@ -1,5 +1,5 @@
 var stage = new createjs.Stage("lineDate");
-var clientWidth = document.querySelector("body").clientWidth;
+var clientWidth = document.querySelector("body").clientWidth-1;
 console.log(clientWidth);
 var canvas = document.getElementById("lineDate");
 var angle = [2 * Math.PI * 100, 2 * Math.PI * 100, 2 * Math.PI * 100];
@@ -9,7 +9,7 @@ var shift_top = 20;
 var circle_size = 15;
 
 window.onresize = function() {
-    clientWidth = document.querySelector("body").clientWidth;
+    clientWidth = document.querySelector("body").clientWidth-1;
     cir_shape = [];
     stage = new createjs.Stage("lineDate");
     draw_init();
@@ -23,19 +23,28 @@ function draw_init(){
 
     for(let i=0;i<3;i++){
         var circle = new createjs.Shape();
-        circle.graphics.beginFill("white").drawCircle(clientWidth/6 * (i*2+1), shift_top, circle_size);
         circle.x = clientWidth/6 * (i*2+1);
+        circle.graphics.beginFill("white").drawCircle(circle.x, shift_top, circle_size);
         circle.anim = false;
-        circle.setTransform(clientWidth/6 * (i*2+1), shift_top, 1, 1, 0, 0, 0, clientWidth/6 * (i*2+1), shift_top);
+        circle.lock = false;
+        circle.setTransform(circle.x, shift_top, 1, 1, 0, 0, 0, circle.x, shift_top);
         //circle.addEventListener("mouseover", activeAnim);
         //circle.addEventListener("mouseout", function(e){e.target.anim = false;});
         stage.addChild(circle);
         cir_shape.push(circle);
+        console.log(circle);
+        console.log(circle.graphics.command.x);
     }
 
     base_line = new createjs.Shape();
     base_line.graphics.beginFill("white").drawRect(0, shift_top - 3, clientWidth, 5);;
     stage.addChild(base_line);
+
+    var polygon = new createjs.Shape();
+    polygon.graphics.beginStroke("white");
+    polygon.graphics.beginFill("white").moveTo(clientWidth/2-10, 60).lineTo(clientWidth/2+10, 60).lineTo(clientWidth/2, 40).lineTo(clientWidth/2-10, 60);
+    stage.addChild(polygon);
+    console.log(polygon);
 
     createjs.Ticker.framerate=60; 
     createjs.Ticker.addEventListener("tick", tick);
@@ -44,6 +53,10 @@ function draw_init(){
 
 function tick(event) {    
     for(let i=0;i<3;i++){
+        if(cir_shape[0].lock){
+            if(angle[i] != Math.PI * 100)
+                angle[i] = 2 * Math.PI * 100 - 10;
+        }
         var scale = Math.cos(angle[i]/100);
         if(angle[i] < 2 * Math.PI * 100 || cir_shape[i].anim){
             cir_shape[i].setTransform(cir_shape[i].x,shift_top, Math.abs(scale+1)/4 + 0.5, Math.abs(scale+1)/4 + 0.5,0,0,0,cir_shape[i].x,shift_top);
